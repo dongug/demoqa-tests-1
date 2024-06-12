a52f7518f371b264a4a010132deb336b952ac329
Akmal Rizaev <testing300bucks@gmail.com>
2024-06-12 18:23:19 +0500

    Some changes in TestBoxTests

--- src/test/java/TestBoxTests.java
+++ src/test/java/TestBoxTests.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -11,7 +13,7 @@ public class TestBoxTests {
     void fillFormTest() {
         open("https://demoqa.com/text-box");
 
-        $("#userName").setValue("Alexei Egorov");
+        $("#userName").shouldBe(visible).setValue("Alexei Egorov");
         $("#userEmail").setValue("alexeiegorov@gmail.com");
         $("#currentAddress").setValue("Some address 1");
         $("#permanentAddress").setValue("Another address 2");
@@ -20,5 +22,23 @@ public class TestBoxTests {
                 text("Some address 1"), text("Another address 2"));
 
 
+    }
+
+    @Test
+    void fillAnotherFormTest() {
+        open("https://demoqa.com/automation-practice-form");
+
+        $("#firstName").setValue("Georgiy");
+        $("#lastName").setValue("Pobedonosets");
+        $("#userEmail").setValue("geor@pobed.com");
+        $("#gender-radio-3").parent().click();
+        $(byText("Female")).click();
+        $("#userNumber").setValue("1234567899");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1999");
+        $(".react-datepicker__day--004:not(.react-datepicker__day--outside-month)").click();
+//                react-datepicker__day react-datepicker__day--004 react-datepicker__day--outside-month
+//        $("#subjectsContainer > div > div.subjects-auto-complete__indicators.css-1wy0on6").sendKeys("art");
     }
 }
